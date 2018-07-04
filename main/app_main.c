e0b9f8f6b07306d9a40e31695c448ab480900a22
Yeon-Jae Jang <yeoncomi@gmail.com>
2018-07-03 22:39:22 -0700

    dlete library

---
+++ main/app_main.c
@@ -0,0 +1,32 @@
+#include "freertos/FreeRTOS.h"
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_event_loop.h"
+#include "esp_event_loop.h"
+#include "nvs_flash.h"
+#include "8bkc-hal.h"
+#include "8bkc-ugui.h"
+#include "powerbtn_menu.h"
+#include "ugui.h"
+#include "ESP32_TwitterAPI.h"
+void app_main(void){
+     nvs_flash_init();
+    tcpip_adapter_init();
+    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    wifi_config_t sta_config = {
+        .sta = {
+            .ssid = "access_point_name",
+            .password = "password",
+            .bssid_set = false
+        }
+    };
+    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+    ESP_ERROR_CHECK( esp_wifi_connect() );
+}
+
